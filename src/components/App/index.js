5f0e4579a46f6b2285c8ad3a7960dfcc597d61be
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2018-04-06 00:39:20 +0530

    bolierplate for hello world

---
+++ src/components/App/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route, withRouter } from 'react-router-dom';
+// import Header from '../Header';
+// import Signup from '../Signup';
+// import Login from '../Login';
+import Welcome from '../Welcome/welcome';
+// import NotFound from '../NotFound404';
+// import './index.css';
+
+const App = () => (
+  <div className="App">
+    {/* <Header /> */}
+    <Switch>
+      <Route exact path="/" component={Welcome} />
+      {/* <Route path="/signup" component={Signup} /> */}
+      {/* <Route path="/login" component={Login} /> */}
+      {/* <Route path="*" exact component={NotFound} /> */}
+    </Switch>
+  </div>
+);
+
+
+export default withRouter(App);
---
+++ src/components/Welcome/welcome.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import './index.css';
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div> Hello World </div>
+    );
+  }
+}
+
+
+export default Welcome;
